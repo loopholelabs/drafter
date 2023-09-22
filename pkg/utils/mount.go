16461719bf61d5ae82965cf301d0c45bbfdcc7b2
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-09-22 21:47:03 +0200

    fix: Don't sync before unmounting since `umount` automatically does that

--- pkg/utils/mount.go
+++ pkg/utils/mount.go
@@ -1,8 +1,6 @@
 package utils
 
 import (
-	"os"
-
 	"golang.org/x/sys/unix"
 )
 
@@ -16,27 +14,10 @@ func NewMount(devicePath, dir string) *Mount {
 }
 
 func (m *Mount) Open() error {
-	if err := os.MkdirAll(m.dir, os.ModePerm); err != nil {
-		return err
-	}
-
 	return unix.Mount(m.devicePath, m.dir, "ext4", 0, "")
 }
 
 func (m *Mount) Close() error {
-	fd, err := unix.Open(m.dir, unix.O_RDONLY, 0)
-	if err != nil {
-		return err
-	}
-	defer unix.Close(fd)
-
-	if err := unix.Fsync(fd); err != nil {
-		return err
-	}
-
-	if err := unix.Unmount(m.dir, 0); err != nil {
-		return err
-	}
-
-	return os.RemoveAll(m.dir)
+	// No need to sync here; `unmount` automatically syncs the filesystem
+	return unix.Unmount(m.dir, 0)
 }
