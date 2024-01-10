fa47d315e480b33bcc9a215c44b6d558049d365c
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-01-10 18:15:04 +0100

    fix: Handle plain closed error in `IsClosedErr` check

--- pkg/utils/closed.go
+++ pkg/utils/closed.go
@@ -9,7 +9,7 @@ import (
 )
 
 func IsClosedErr(err error) bool {
-	if errors.Is(err, io.EOF) || errors.Is(err, net.ErrClosed) || errors.Is(err, syscall.ECONNRESET) || strings.HasSuffix(err.Error(), "read: connection timed out") || strings.HasSuffix(err.Error(), "write: broken pipe") || strings.HasSuffix(err.Error(), "unexpected EOF") || strings.HasSuffix(err.Error(), "use of closed network connection") || strings.HasSuffix(err.Error(), "grpc: the server has been stopped") || strings.HasSuffix(err.Error(), "grpc: the client connection is closing") {
+	if errors.Is(err, io.EOF) || errors.Is(err, net.ErrClosed) || errors.Is(err, syscall.ECONNRESET) || strings.HasSuffix(err.Error(), "read: connection timed out") || strings.HasSuffix(err.Error(), "write: broken pipe") || strings.HasSuffix(err.Error(), "unexpected EOF") || strings.HasSuffix(err.Error(), "use of closed network connection") || strings.HasSuffix(err.Error(), "grpc: the server has been stopped") || strings.HasSuffix(err.Error(), "grpc: the client connection is closing") || strings.HasSuffix(err.Error(), "closed") {
 		return true
 	}
 
