9b3d36ea6a2cdb9dd930c1549289bca7dc225e10
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-04-22 18:07:32 -0700

    feat: Add `context.Context` support to liveness receiver

--- pkg/vsock/liveness_receiver.go
+++ pkg/vsock/liveness_receiver.go
@@ -1,9 +1,16 @@
 package vsock
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net"
 	"os"
+	"sync"
+)
+
+var (
+	errFinished = errors.New("finished")
 )
 
 type LivenessPingReceiver struct {
@@ -31,10 +38,55 @@ func (l *LivenessPingReceiver) Open() (string, error) {
 	return l.vsockPortPath, nil
 }
 
-func (l *LivenessPingReceiver) Receive() error {
+func (l *LivenessPingReceiver) ReceiveAndClose(ctx context.Context) (errs []error) {
+	var errsLock sync.Mutex
+
+	var wg sync.WaitGroup
+	defer wg.Wait()
+
+	ctx, cancel := context.WithCancelCause(ctx)
+	defer cancel(errFinished)
+
+	handleGoroutinePanic := func() func() {
+		return func() {
+			if err := recover(); err != nil {
+				errsLock.Lock()
+				defer errsLock.Unlock()
+
+				var e error
+				if v, ok := err.(error); ok {
+					e = v
+				} else {
+					e = fmt.Errorf("%v", err)
+				}
+
+				if !(errors.Is(e, context.Canceled) && errors.Is(context.Cause(ctx), errFinished)) {
+					errs = append(errs, e)
+				}
+
+				cancel(errFinished)
+			}
+		}
+	}
+
+	defer handleGoroutinePanic()()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer handleGoroutinePanic()()
+
+		// Cause the `Accept()` function to unblock
+		<-ctx.Done()
+
+		_ = l.lis.Close() // We ignore errors here since we might interrupt a network connection
+	}()
+
+	defer l.Close()
+
 	conn, err := l.lis.Accept()
 	if err != nil {
-		return err
+		panic(err)
 	}
 	defer conn.Close()
 
