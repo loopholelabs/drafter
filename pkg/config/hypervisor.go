232a15c95301edf5937baca202702dadb34ec701
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-04-25 20:54:48 -0700

    chore: Drop kernel log rate limiting

--- pkg/config/hypervisor.go
+++ pkg/config/hypervisor.go
@@ -1,7 +1,7 @@
 package config
 
 const (
-	DefaultBootArgs = "console=ttyS0 panic=1 pci=off modules=ext4 rootfstype=ext4 i8042.noaux i8042.nomux i8042.nopnp i8042.dumbkbd rootflags=rw"
+	DefaultBootArgs = "console=ttyS0 panic=1 pci=off modules=ext4 rootfstype=ext4 i8042.noaux i8042.nomux i8042.nopnp i8042.dumbkbd rootflags=rw printk.devkmsg=on printk_ratelimit=0 printk_ratelimit_burst=0"
 )
 
 type HypervisorConfiguration struct {
