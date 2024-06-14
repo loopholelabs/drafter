acc996af318aa262e6cf660ba8217dc5d44af476
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-06-13 20:01:47 -0700

    refactor: Use label-less identification method for the root disk to drop requirement on having an initramfs

--- pkg/roles/constants.go
+++ pkg/roles/constants.go
@@ -25,5 +25,5 @@ var (
 )
 
 const (
-	DefaultBootArgs = "console=ttyS0 panic=1 pci=off modules=ext4 rootfstype=ext4 root=LABEL=root i8042.noaux i8042.nomux i8042.nopnp i8042.dumbkbd rootflags=rw printk.devkmsg=on printk_ratelimit=0 printk_ratelimit_burst=0"
+	DefaultBootArgs = "console=ttyS0 panic=1 pci=off modules=ext4 rootfstype=ext4 root=/dev/vda i8042.noaux i8042.nomux i8042.nopnp i8042.dumbkbd rootflags=rw printk.devkmsg=on printk_ratelimit=0 printk_ratelimit_burst=0"
 )
