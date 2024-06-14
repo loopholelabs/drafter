be88ed681bbb0d220fe7f3ce7f92060790138c43
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-06-13 22:01:24 -0700

    refactor: Add soft dependency between agent and liveness

--- os/package/drafter-liveness/drafter-liveness.mk
+++ os/package/drafter-liveness/drafter-liveness.mk
@@ -4,8 +4,6 @@
 #
 ################################################################################
 
-DRAFTER_LIVENESS_DEPENDENCIES = drafter-agent
-
 DRAFTER_LIVENESS_VERSION = 5ef1735c45a277f39c3d0df1ba42d5777e304b5a
 DRAFTER_LIVENESS_SITE = $(call github,loopholelabs,drafter,$(DRAFTER_LIVENESS_VERSION))
 
@@ -19,6 +17,7 @@ DRAFTER_LIVENESS_BUILD_TARGETS = cmd/drafter-liveness
 define DRAFTER_LIVENESS_INSTALL_INIT_SYSTEMD
 	mkdir -p $(TARGET_DIR)/usr/lib/systemd/system
 	sed -e 's%@DRAFTER_LIVENESS_VSOCK_PORT@%$(BR2_PACKAGE_DRAFTER_LIVENESS_VSOCK_PORT)%g' \
+		-e 's%@DRAFTER_LIVENESS_SYSTEMD_DEPENDENCY@%$(BR2_PACKAGE_DRAFTER_LIVENESS_SYSTEMD_DEPENDENCY)%g' \
 		$(DRAFTER_LIVENESS_PKGDIR)/drafter-liveness.service.in \
 		> $(TARGET_DIR)/usr/lib/systemd/system/drafter-liveness.service
 endef
