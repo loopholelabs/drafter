afe8f07cdddc6e565b0ceff6731fd07533a957c8
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-06-13 21:53:33 -0700

    feat: Add soft configurable service dependency between agent and an additional service

--- os/package/drafter-agent/drafter-agent.mk
+++ os/package/drafter-agent/drafter-agent.mk
@@ -17,6 +17,7 @@ DRAFTER_AGENT_BUILD_TARGETS = cmd/drafter-agent
 define DRAFTER_AGENT_INSTALL_INIT_SYSTEMD
 	mkdir -p $(TARGET_DIR)/usr/lib/systemd/system
 	sed -e 's%@DRAFTER_AGENT_VSOCK_PORT@%$(BR2_PACKAGE_DRAFTER_AGENT_VSOCK_PORT)%g' \
+		-e 's%@DRAFTER_AGENT_SYSTEMD_DEPENDENCY@%$(BR2_PACKAGE_DRAFTER_AGENT_SYSTEMD_DEPENDENCY)%g' \
 		$(DRAFTER_AGENT_PKGDIR)/drafter-agent.service.in \
 		> $(TARGET_DIR)/usr/lib/systemd/system/drafter-agent.service
 endef
