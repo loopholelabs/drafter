a60494b584f506d8c2fa79f006f4351e13b5b255
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-11-10 20:23:30 -0800

    fix: Add cmdline flags to allow heterogenous host migrations
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- pkg/snapshotter/constants.go
+++ pkg/snapshotter/constants.go
@@ -3,5 +3,5 @@ package snapshotter
 const (
 	VSockName = "vsock.sock"
 
-	DefaultBootArgs = "console=ttyS0 panic=1 pci=off modules=ext4 rootfstype=ext4 root=/dev/vda i8042.noaux i8042.nomux i8042.nopnp i8042.dumbkbd rootflags=rw printk.devkmsg=on printk_ratelimit=0 printk_ratelimit_burst=0 clocksource=tsc random.trust_cpu=on"
+	DefaultBootArgs = "console=ttyS0 panic=1 pci=off modules=ext4 rootfstype=ext4 root=/dev/vda i8042.noaux i8042.nomux i8042.nopnp i8042.dumbkbd rootflags=rw printk.devkmsg=on printk_ratelimit=0 printk_ratelimit_burst=0 clocksource=tsc nokaslr lapic=notscdeadline tsc=unstable"
 )
