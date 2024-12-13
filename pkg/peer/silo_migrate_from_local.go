4498bacc14afe5d088c6b7c6875dd85fa022ad37
Jimmy Moore <jamesmoore@loopholelabs.io>
2024-12-13 18:35:09 +0000

    Refactor silo dev schema for local
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

--- pkg/peer/silo_migrate_from_local.go
+++ pkg/peer/silo_migrate_from_local.go
@@ -6,12 +6,9 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"sync/atomic"
 
-	"github.com/loopholelabs/drafter/internal/utils"
 	"github.com/loopholelabs/drafter/pkg/mounter"
 	"github.com/loopholelabs/goroutine-manager/pkg/manager"
-	"github.com/loopholelabs/silo/pkg/storage"
 	"github.com/loopholelabs/silo/pkg/storage/config"
 	"github.com/loopholelabs/silo/pkg/storage/device"
 )
@@ -25,100 +22,95 @@ type MigrateFromStage struct {
 	BlockSize uint32
 }
 
+func SiloCreateDevSchema(i *MigrateFromStage) (*config.DeviceSchema, error) {
+	stat, err := os.Stat(i.Base)
+	if err != nil {
+		return nil, errors.Join(mounter.ErrCouldNotGetBaseDeviceStat, err)
+	}
+
+	ds := &config.DeviceSchema{
+		Name:      i.Name,
+		BlockSize: fmt.Sprintf("%v", i.BlockSize),
+		Expose:    true,
+		Size:      fmt.Sprintf("%v", stat.Size()),
+	}
+	if strings.TrimSpace(i.Overlay) == "" || strings.TrimSpace(i.State) == "" {
+		ds.System = "file"
+		ds.Location = i.Base
+	} else {
+		err := os.MkdirAll(filepath.Dir(i.Overlay), os.ModePerm)
+		if err != nil {
+			return nil, errors.Join(mounter.ErrCouldNotCreateOverlayDirectory, err)
+		}
+
+		err = os.MkdirAll(filepath.Dir(i.State), os.ModePerm)
+		if err != nil {
+			return nil, errors.Join(mounter.ErrCouldNotCreateStateDirectory, err)
+		}
+
+		ds.System = "sparsefile"
+		ds.Location = i.Overlay
+
+		ds.ROSource = &config.DeviceSchema{
+			Name:     i.State,
+			System:   "file",
+			Location: i.Base,
+			Size:     fmt.Sprintf("%v", stat.Size()),
+		}
+	}
+	return ds, nil
+}
+
 func SiloMigrateFromLocal(devices []*MigrateFromStage, goroutineManager *manager.GoroutineManager, hooks mounter.MigrateFromHooks,
 	stageOutputCb func(mfs migrateFromStage),
 	addDeviceCloseFuncSingle func(f func() error),
 	exposedCb func(index int, name string, devicePath string) error,
 ) error {
-	// Use an atomic counter instead of a WaitGroup so that we can wait without leaking a goroutine
-	var remainingRequestedLocalDevices atomic.Int32
-	remainingRequestedLocalDevices.Add(int32(len(devices)))
 
-	_, deferFuncs, err := utils.ConcurrentMap(
-		devices,
-		func(_ int, input *MigrateFromStage, _ *struct{}, addDefer func(deferFunc func() error)) error {
-			if hook := hooks.OnLocalDeviceRequested; hook != nil {
-				hook(uint32(input.Id), input.Name)
-			}
+	// First create a set of schema for the devices...
+	siloDevices := make([]*config.DeviceSchema, 0)
+	for _, i := range devices {
+		ds, err := SiloCreateDevSchema(i)
+		if err != nil {
+			return err
+		}
+		siloDevices = append(siloDevices, ds)
+		fmt.Printf("%s\n", ds.EncodeAsBlock())
+	}
 
-			if remainingRequestedLocalDevices.Add(-1) <= 0 {
-				if hook := hooks.OnLocalAllDevicesRequested; hook != nil {
-					hook()
-				}
-			}
+	// Create the devices...
+	for index, input := range devices {
+		if hook := hooks.OnLocalDeviceRequested; hook != nil {
+			hook(uint32(input.Id), input.Name)
+		}
 
-			stat, err := os.Stat(input.Base)
-			if err != nil {
-				return errors.Join(mounter.ErrCouldNotGetBaseDeviceStat, err)
-			}
+		ds := siloDevices[index]
+		local, dev, err := device.NewDevice(ds)
 
-			var (
-				local storage.Provider
-				dev   storage.ExposedStorage
-			)
-			if strings.TrimSpace(input.Overlay) == "" || strings.TrimSpace(input.State) == "" {
-				local, dev, err = device.NewDevice(&config.DeviceSchema{
-					Name:      input.Name,
-					System:    "file",
-					Location:  input.Base,
-					Size:      fmt.Sprintf("%v", stat.Size()),
-					BlockSize: fmt.Sprintf("%v", input.BlockSize),
-					Expose:    true,
-				})
-			} else {
-				if err := os.MkdirAll(filepath.Dir(input.Overlay), os.ModePerm); err != nil {
-					return errors.Join(mounter.ErrCouldNotCreateOverlayDirectory, err)
-				}
+		if err != nil {
+			return errors.Join(mounter.ErrCouldNotCreateLocalDevice, err)
+		}
+		addDeviceCloseFuncSingle(local.Close)
+		addDeviceCloseFuncSingle(dev.Shutdown)
 
-				if err := os.MkdirAll(filepath.Dir(input.State), os.ModePerm); err != nil {
-					return errors.Join(mounter.ErrCouldNotCreateStateDirectory, err)
-				}
+		stageOutputCb(migrateFromStage{
+			name:      input.Name,
+			blockSize: input.BlockSize,
+			id:        uint32(input.Id),
+			remote:    false,
+			storage:   local,
+			device:    dev,
+		})
 
-				local, dev, err = device.NewDevice(&config.DeviceSchema{
-					Name:      input.Name,
-					System:    "sparsefile",
-					Location:  input.Overlay,
-					Size:      fmt.Sprintf("%v", stat.Size()),
-					BlockSize: fmt.Sprintf("%v", input.BlockSize),
-					Expose:    true,
-					ROSource: &config.DeviceSchema{
-						Name:     input.State,
-						System:   "file",
-						Location: input.Base,
-						Size:     fmt.Sprintf("%v", stat.Size()),
-					},
-				})
-			}
-			if err != nil {
-				return errors.Join(mounter.ErrCouldNotCreateLocalDevice, err)
-			}
-			addDefer(local.Close)
-			addDefer(dev.Shutdown)
-
-			dev.SetProvider(local)
-
-			stageOutputCb(migrateFromStage{
-				name:      input.Name,
-				blockSize: input.BlockSize,
-				id:        uint32(input.Id),
-				remote:    false,
-				storage:   local,
-				device:    dev,
-			})
-
-			devicePath := filepath.Join("/dev", dev.Device())
-
-			return exposedCb(input.Id, input.Name, devicePath)
-
-		},
-	)
-
-	// Make sure that we schedule the `deferFuncs` even if we get an error during device setup
-	for _, deferFuncs := range deferFuncs {
-		for _, deferFunc := range deferFuncs {
-			addDeviceCloseFuncSingle(deferFunc)
+		err = exposedCb(input.Id, input.Name, filepath.Join("/dev", dev.Device()))
+		if err != nil {
+			return err
 		}
 	}
 
-	return err
+	if hook := hooks.OnLocalAllDevicesRequested; hook != nil {
+		hook()
+	}
+
+	return nil
 }
