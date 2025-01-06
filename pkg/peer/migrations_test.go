48358ad8240830a09a572669e23869952ebb2727
Jimmy Moore <jamesmoore@loopholelabs.io>
2025-01-06 12:36:54 +0000

    First simple migration unit test
    Signed-off-by: Jimmy Moore <jamesmoore@loopholelabs.io>

--- pkg/peer/migrations_test.go
+++ pkg/peer/migrations_test.go
@@ -1,14 +1,21 @@
 package peer
 
 import (
+	"context"
 	"crypto/rand"
 	"fmt"
+	"io"
 	"os"
 	"os/user"
 	"path"
+	"strings"
+	"sync"
 	"testing"
+	"time"
 
+	"github.com/loopholelabs/drafter/pkg/mounter"
 	"github.com/loopholelabs/silo/pkg/storage/devicegroup"
+	"github.com/loopholelabs/silo/pkg/storage/migrator"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -57,10 +64,10 @@ func setupFromFs(t *testing.T) (string, *devicegroup.DeviceGroup) {
 	err = fd.Close()
 	assert.NoError(t, err)
 
-	err = dg.CloseAll()
-	assert.NoError(t, err)
-
 	t.Cleanup(func() {
+		err = dg.CloseAll()
+		assert.NoError(t, err)
+
 		os.Remove(tdir)
 	})
 
@@ -90,13 +97,83 @@ func TestMigrateFromFsThenBetween(t *testing.T) {
 		return
 	}
 
-	/*
-		TODO: Finish this... We should migrate through a pipe
+	// First lets setup a temp dir
+	tdir2, err := os.MkdirTemp("", "drafter_vm_*")
+	assert.NoError(t, err)
+	t.Cleanup(func() {
+		os.Remove(tdir2)
+	})
 
-		tdir, dg := setupFromFs(t)
+	tdir, dg := setupFromFs(t)
 
-		migrateToPipe()
+	r1, w1 := io.Pipe()
+	r2, w2 := io.Pipe()
 
-		migrateFromPipe()
-	*/
+	progress := func(p map[string]*migrator.MigrationProgress) {
+		fmt.Printf("Progress...\n")
+	}
+
+	devices := []mounter.MigrateToDevice{
+		{
+			Name:           "test",
+			MaxDirtyBlocks: 10,
+			MinCycles:      4,
+			MaxCycles:      10,
+			CycleThrottle:  100 * time.Millisecond,
+		},
+	}
+
+	var dg2 *devicegroup.DeviceGroup
+	var migrateWait sync.WaitGroup
+
+	migrateWait.Add(1)
+	go func() {
+		tweak := func(index int, name string, schema string) string {
+			fmt.Printf("Tweak %s\n", schema)
+			s := strings.ReplaceAll(schema, tdir, tdir2)
+			fmt.Printf(" -> %s\n", s)
+			return s
+		}
+
+		dg2, err = migrateFromPipe(nil, nil, tdir2,
+			context.TODO(), []io.Reader{r2}, []io.Writer{w1}, tweak)
+		assert.NoError(t, err)
+
+		// TODO: Check dg2 looks good...
+		names := dg2.GetAllNames()
+		assert.Equal(t, 1, len(names))
+		assert.Equal(t, "test", names[0])
+
+		// TODO: Read data from tdir2 and make sure it matches.
+
+		err = dg2.WaitForCompletion()
+		assert.NoError(t, err)
+
+		migrateWait.Done()
+	}()
+
+	suspendFunc := func(ctx context.Context, timeout time.Duration) error {
+		fmt.Printf("suspendFunc\n")
+		return nil
+	}
+	msyncFunc := func(ctx context.Context) error {
+		fmt.Printf("msyncFunc\n")
+		return nil
+	}
+	onBeforeSuspend := func() {
+		fmt.Printf("onBeforeSuspend\n")
+	}
+	onAfterSuspend := func() {
+		fmt.Printf("onAfterSuspend\n")
+	}
+
+	vmState := NewVMStateMgr(context.TODO(), suspendFunc, 100*time.Millisecond,
+		msyncFunc, onBeforeSuspend, onAfterSuspend)
+
+	err = migrateToPipe(context.TODO(), []io.Reader{r1}, []io.Writer{w2},
+		dg, 100, progress, vmState, devices)
+	assert.NoError(t, err)
+
+	// Make sure it all completed
+	migrateWait.Wait()
 }
