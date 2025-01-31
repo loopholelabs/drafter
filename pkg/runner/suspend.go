4beca3acc5f8a98fd7e7d4d68ff9163371e96374
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-01-30 18:56:17 -0800

    fix: Don't try to run suspend RPCs if resumes failed
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- pkg/runner/suspend.go
+++ pkg/runner/suspend.go
@@ -14,20 +14,29 @@ func (resumedRunner *ResumedRunner[L, R, G]) SuspendAndCloseAgentServer(ctx cont
 	suspendCtx, cancelSuspendCtx := context.WithTimeout(ctx, suspendTimeout)
 	defer cancelSuspendCtx()
 
-	// This is a safe type cast because R is constrained by ipc.AgentServerRemote, so this specific BeforeSuspend field
-	// must be defined or there will be a compile-time error.
-	// The Go Generics system can't catch this here however, it can only catch it once the type is concrete, so we need to manually cast.
-	remote := *(*ipc.AgentServerRemote[G])(unsafe.Pointer(&resumedRunner.acceptingAgent.Remote))
-	if err := remote.BeforeSuspend(suspendCtx); err != nil {
-		return errors.Join(ErrCouldNotCallBeforeSuspendRPC, err)
+	// This `nil` check is required since `SuspendAndCloseAgentServer` can be run even if a runner resume failed
+	if resumedRunner.Remote != nil {
+		// This is a safe type cast because R is constrained by ipc.AgentServerRemote, so this specific BeforeSuspend field
+		// must be defined or there will be a compile-time error.
+		// The Go Generics system can't catch this here however, it can only catch it once the type is concrete, so we need to manually cast.
+		remote := *(*ipc.AgentServerRemote[G])(unsafe.Pointer(&resumedRunner.acceptingAgent.Remote))
+		if err := remote.BeforeSuspend(suspendCtx); err != nil {
+			return errors.Join(ErrCouldNotCallBeforeSuspendRPC, err)
+		}
 	}
 
 	// Connections need to be closed before creating the snapshot
-	if err := resumedRunner.acceptingAgent.Close(); err != nil {
-		return errors.Join(snapshotter.ErrCouldNotCloseAcceptingAgent, err)
+	// This `nil` check is required since `SuspendAndCloseAgentServer` can be run even if a runner resume failed
+	if resumedRunner.acceptingAgent != nil {
+		if err := resumedRunner.acceptingAgent.Close(); err != nil {
+			return errors.Join(snapshotter.ErrCouldNotCloseAcceptingAgent, err)
+		}
 	}
 
-	resumedRunner.agent.Close()
+	// This `nil` check is required since `SuspendAndCloseAgentServer` can be run even if a runner resume failed
+	if resumedRunner.agent != nil {
+		resumedRunner.agent.Close()
+	}
 
 	if err := resumedRunner.createSnapshot(suspendCtx); err != nil {
 		return errors.Join(snapshotter.ErrCouldNotCreateSnapshot, err)
