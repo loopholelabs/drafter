86e278a6f2c045917bd64374be4fa59fb85e5787
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-03-05 00:13:38 -0800

    fix: Use `-ex` and `-j` properly in build scripts
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- hack/remake.sh
+++ hack/remake.sh
@@ -3,9 +3,9 @@
 set -ex
 
 make -j$(nproc)
-sudo make install j$(nproc)
-sudo killall drafter-peer
-sudo killall firecracker
+sudo make install -j$(nproc)
+sudo killall drafter-peer || true
+sudo killall firecracker || true
 sudo rm -rf out/instance-*
 
 #mkdir out/instance-1
