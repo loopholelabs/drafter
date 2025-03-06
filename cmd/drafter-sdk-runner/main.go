7636edf5b1f15c90eecf20e8d75c9c8390435877
Felicitas Pojtinger <felicitas@pojtinger.com>
2025-03-05 16:29:21 -0800

    feat: Add minimal copy-based runner for SDK
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- cmd/drafter-sdk-runner/main.go
+++ cmd/drafter-sdk-runner/main.go
@@ -19,63 +19,43 @@ func main() {
 	defer cancel()
 
 	config := firecracker.Config{
-		LogLevel:        "Debug",
-		KernelImagePath: "out/blueprint/vmlinux",
-		KernelArgs:      "console=ttyS0 panic=1 pci=off modules=ext4 rootfstype=ext4 root=/dev/vda i8042.noaux i8042.nomux i8042.nopnp i8042.dumbkbd rootflags=rw printk.devkmsg=on printk_ratelimit=0 printk_ratelimit_burst=0 clocksource=tsc nokaslr lapic=notscdeadline tsc=unstable",
-		Drives: firecracker.
-			NewDrivesBuilder("out/blueprint/rootfs.ext4").
-			AddDrive("out/blueprint/oci.ext4", false).
-			Build(),
+		LogLevel: "Debug",
 		MachineCfg: models.MachineConfiguration{
 			VcpuCount:  firecracker.Int64(1),
 			MemSizeMib: firecracker.Int64(512),
 		},
 		NetNS: "/var/run/netns/ark0",
-		VsockDevices: []firecracker.VsockDevice{
-			{
-				Path: "vsock.sock",
-				CID:  3,
-			},
-		},
 		JailerCfg: &firecracker.JailerConfig{
 			UID:            firecracker.Int(0),
 			GID:            firecracker.Int(0),
 			ID:             shortuuid.New(),
 			NumaNode:       firecracker.Int(0),
 			ChrootBaseDir:  "out/vms",
-			ChrootStrategy: firecracker.NewNaiveChrootStrategy("out/blueprint/vmlinux"),
+			ChrootStrategy: firecracker.NewNaiveChrootStrategy("out/package/vmlinux"),
 			ExecFile:       "/usr/local/bin/firecracker",
 			JailerBinary:   "/usr/local/bin/jailer",
 			CgroupVersion:  *firecracker.String("2"),
 		},
-		NetworkInterfaces: firecracker.NetworkInterfaces{
-			{
-				StaticConfiguration: &firecracker.StaticNetworkConfiguration{
-					MacAddress:  "02:0e:d9:fd:68:3d",
-					HostDevName: "tap0",
-				},
-			},
+		Snapshot: firecracker.SnapshotConfig{
+			ResumeVM: true,
 		},
+		DisableValidation: true,
 	}
 
 	if err := os.MkdirAll(filepath.Join(config.JailerCfg.ChrootBaseDir, "firecracker", config.JailerCfg.ID, "root"), os.ModePerm); err != nil {
 		panic(err)
 	}
 
-	machine, err := firecracker.NewMachine(ctx, config, firecracker.WithLogger(logrus.NewEntry(logrus.New())))
+	machine, err := firecracker.NewMachine(
+		ctx,
+		config,
+		firecracker.WithLogger(logrus.NewEntry(logrus.New())),
+		firecracker.WithSnapshot(
+			"memory.bin",
+			"state.bin",
+		),
+	)
 	if err != nil {
-		panic(err)
-	}
-
-	livenessSocketPath := filepath.Join(machine.Cfg.JailerCfg.ChrootBaseDir, "firecracker", machine.Cfg.JailerCfg.ID, "root", "vsock.sock_25")
-
-	livenessLis, err := net.Listen("unix", livenessSocketPath)
-	if err != nil {
-		panic(err)
-	}
-	defer livenessLis.Close()
-
-	if err := os.Chown(livenessSocketPath, *config.JailerCfg.UID, *config.JailerCfg.GID); err != nil {
 		panic(err)
 	}
 
@@ -91,6 +71,41 @@ func main() {
 		panic(err)
 	}
 
+	if err := copy.Copy(
+		"out/package/vmlinux",
+		filepath.Join(machine.Cfg.JailerCfg.ChrootBaseDir, "firecracker", machine.Cfg.JailerCfg.ID, "root", "vmlinux"),
+	); err != nil {
+		panic(err)
+	}
+
+	if err := copy.Copy(
+		"out/package/rootfs.ext4",
+		filepath.Join(machine.Cfg.JailerCfg.ChrootBaseDir, "firecracker", machine.Cfg.JailerCfg.ID, "root", "rootfs.ext4"),
+	); err != nil {
+		panic(err)
+	}
+
+	if err := copy.Copy(
+		"out/package/oci.ext4",
+		filepath.Join(machine.Cfg.JailerCfg.ChrootBaseDir, "firecracker", machine.Cfg.JailerCfg.ID, "root", "oci.ext4"),
+	); err != nil {
+		panic(err)
+	}
+
+	if err := copy.Copy(
+		"out/package/memory.bin",
+		filepath.Join(machine.Cfg.JailerCfg.ChrootBaseDir, "firecracker", machine.Cfg.JailerCfg.ID, "root", "memory.bin"),
+	); err != nil {
+		panic(err)
+	}
+
+	if err := copy.Copy(
+		"out/package/state.bin",
+		filepath.Join(machine.Cfg.JailerCfg.ChrootBaseDir, "firecracker", machine.Cfg.JailerCfg.ID, "root", "state.bin"),
+	); err != nil {
+		panic(err)
+	}
+
 	if err := machine.Start(ctx); err != nil {
 		panic(err)
 	}
@@ -101,10 +116,6 @@ func main() {
 			panic(err)
 		}
 	}()
-
-	if _, err = livenessLis.Accept(); err != nil {
-		panic(err)
-	}
 
 	if _, err = agentLis.Accept(); err != nil {
 		panic(err)
